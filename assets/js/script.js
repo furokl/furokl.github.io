ca0d9aa4ca3eb3456f3b05da88b89f2c7ab3226f
furokl <70150974+furokl@users.noreply.github.com>
2024-07-22 09:17:53 +0300

    tost3

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function showSection(sectionId) {
+    var sections = document.getElementsByClassName('section');
+    for (var i = 0; i < sections.length; i++) {
+        sections[i].style.display = 'none';
+    }
+    document.getElementById(sectionId).style.display = 'block';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    showSection('home');
+});
